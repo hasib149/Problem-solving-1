4d81add7aa1dcd50d5fcc0a5c501dff99decddae
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2026-01-08 14:42:17 +0600

    problem 1 solve

---
+++ index.js
@@ -0,0 +1,10 @@
+// Problem 1: Reverse a String
+function reverseString(str) {
+  let reverse = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  return reverse;
+}
+console.log(reverseString("shrabon"));
