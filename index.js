7c268df5e3c2d7f47fcf81eaa127f5240ffa99ff
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2026-01-08 15:56:31 +0600

    problem 10 solved

--- index.js
+++ index.js
@@ -120,3 +120,21 @@ function factorial(n) {
 
 console.log(factorial(5));
 console.log(factorial(0));
+
+// Problem 10: PingPong Challenge
+
+function pingPong() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+pingPong();
