bac0515f05d9e18177cdfa92f821ea445661db11
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2026-01-08 15:51:16 +0600

    problem 7 done

--- index.js
+++ index.js
@@ -76,3 +76,17 @@ function sum(arr) {
 }
 
 console.log(sum([1, 2, 3, 4, 5]));
+
+// Problem 7: Find Even Numbers in an Array
+
+function findEvenNumbers(arr) {
+  let evenNumbers = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenNumbers.push(arr[i]);
+    }
+  }
+  return evenNumbers;
+}
+console.log(findEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8]));
